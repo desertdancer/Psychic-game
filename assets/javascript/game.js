93ed75961a8c4d2d7101b8768c47d5b1b84d6218
desertdancer <steviepop@icloud.com>
2019-12-22 14:03:34 -0700

    postT

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,18 @@
-cd ..cd
+var wins=0
+var losses=0
+var guessesLeft=10
+var guessesSofar=[]  // 
+var letters=['a', 'b', 'c', 'd', 'e']
+
+
+
+
+// computer choose a letter
+// user clicking 
+document.onkeydown=function(event){
+console.log(event.key)
+
+
+}
+
+
